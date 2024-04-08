0ac9cfa2c45fa57272ff881da25482941e552534
wglao <wesleygarlao@gmail.com>
2024-04-08 15:32:41 -0500

    correct trunk branch mlp

--- models/models.py
+++ models/models.py
@@ -39,7 +39,7 @@ class ResNet(nn.Module):
         return x
 
 class TrunkNet(nn.Module):
-    def __init__(self, in_size, hidden_size, basis_dims) -> None:
+    def __init__(self, in_size, hidden_size, basis_dims, layers) -> None:
         super().__init__()
         lift = torch.empty((hidden_size,))
         lift_bias = torch.empty((in_size,1))
@@ -47,7 +47,7 @@ class TrunkNet(nn.Module):
         nn.init.kaiming_uniform_(lift_bias)
         self.lift = nn.Parameter(lift)
         self.lift_bias = nn.Parameter(lift_bias)
-        self.body = MLP(hidden_size,hidden_size,basis_dims)
+        self.body = MLP(hidden_size,hidden_size,basis_dims,layers-1)
     
     def forward(self, t):
         v = F.relu(torch.outer(t,self.lift) + self.lift_bias)
