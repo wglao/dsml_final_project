8a494c48d7855709eed6bea4440a4393bc671aa7
wglao <wesleygarlao@gmail.com>
2024-04-09 12:58:33 -0500

    Siren and Siren TrunkNet

--- models/models.py
+++ models/models.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class MLP(nn.Module):
-    def __init__(self, in_size, hidden_size, out_size, layers) -> None:
+    def __init__(self, in_size, hidden_size, out_size, layers):
         super().__init__()
         self.lin_in = nn.Linear(in_size, hidden_size)
         hidden_list = [nn.Linear(hidden_size, hidden_size) for _ in range(layers)]
@@ -18,8 +18,28 @@ class MLP(nn.Module):
         x = self.lin_out(x)
         return x
 
+class Siren(nn.Module):
+    def __init__(self, in_size, hidden_size, out_size, layers, freq_mod: float = 30.):
+        super().__init__()
+        self.lin_in = nn.Linear(in_size, hidden_size)
+
+        hidden_freqs =  freq_mod*torch.ones((layers,1,1))
+        self.hidden_freqs = nn.Parameter(hidden_freqs)
+        
+        hidden_list = [nn.Linear(hidden_size, hidden_size) for _ in range(layers)]
+        self.hidden_list = nn.ModuleList(hidden_list)
+        self.lin_out = nn.Linear(hidden_size, out_size)
+    
+    def forward(self, x):
+        x = torch.sin(self.lin_in(x))
+        for hidden, a in zip(self.hidden_list, self.hidden_freqs):
+            x = torch.sin(hidden(a*x))
+        x = self.lin_out(x)
+        return x
+
+
 class ResNet(nn.Module):
-    def __init__(self, in_size, hidden_size, out_size, layers) -> None:
+    def __init__(self, in_size, hidden_size, out_size, layers):
         super().__init__()
         self.lin_in = nn.Linear(in_size, hidden_size)
         hidden_list = [nn.Linear(hidden_size, hidden_size) for _ in range(layers)]
@@ -34,7 +54,7 @@ class ResNet(nn.Module):
         return x
 
 class TrunkNet(nn.Module):
-    def __init__(self, in_size, hidden_size, basis_dims, layers) -> None:
+    def __init__(self, in_size, hidden_size, basis_dims, layers):
         super().__init__()
         lift = torch.empty((hidden_size,1))
         lift_bias = torch.empty((1,hidden_size))
@@ -48,10 +68,40 @@ class TrunkNet(nn.Module):
         v = F.relu((t @ self.lift.T) + self.lift_bias)
         v = self.body(v)
         return v
+    
+class SirenTrunk(nn.Module):
+    def __init__(self, in_size, basis_dims, layers, freq_mod: float=30.):
+        super().__init__()
+        lift = torch.empty((basis_dims,1))
+        lift_bias = torch.empty((1,basis_dims))
+        recomb_weights = torch.empty((layers+1,1,1))
+        nn.init.kaiming_uniform_(lift)
+        nn.init.kaiming_uniform_(lift_bias)
+        nn.init.kaiming_uniform_(recomb_weights)
+        self.lift = nn.Parameter(lift)
+        self.lift_bias = nn.Parameter(lift_bias)
+        self.recomb_weights = nn.Parameter(recomb_weights)
+        hidden_freqs =  freq_mod*torch.ones((layers,1,1))
+        self.hidden_freqs = nn.Parameter(hidden_freqs)
+
+        hidden_list = [nn.Linear(basis_dims, basis_dims) for _ in range(layers)]
+        self.hidden_list = nn.ModuleList(hidden_list)
+        self.bias = nn.Parameter(torch.zeros([1,1]))
+    
+    def forward(self,t):
+        x = torch.sin((t @ self.lift.T) + self.lift_bias)
+        v = self.recomb_weights[0]*x
+
+        for hidden, a, w in zip(self.hidden_list, self.hidden_freqs, self.recomb_weights[1:]):
+            x = torch.sin(hidden(a*x))
+            v += w*x
+
+        v += self.bias
+        return v
 
 
 class DeepONet(nn.Module):
-    def __init__(self, branch_dict: dict, trunk_dict: dict) -> None:
+    def __init__(self, branch_dict: dict, trunk_dict: dict):
         super().__init__()
         self.branch_net = branch_dict["Net"](**branch_dict["Args"])
         self.trunk_net = trunk_dict["Net"](**trunk_dict["Args"])
