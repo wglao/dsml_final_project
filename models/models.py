0cb4fcd15bb6bef4e4502ea4e8a3cd6c0456ef2a
wglao <wesleygarlao@gmail.com>
2024-04-09 13:24:25 -0500

    handle transpose

--- models/models.py
+++ models/models.py
@@ -111,6 +111,8 @@ class DeepONet(nn.Module):
         v = self.trunk_net(t)
         if len(c.shape) == 1:
             y = v @ c[:,None]
+        elif c.shape[1] == v.shape[1]:
+            y = v @ c.T
         else:
             y = v @ c
         return y
