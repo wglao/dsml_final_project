ca28b6cdef114d35e22f335b492c5b6bc444a3fb
wglao <wesleygarlao@gmail.com>
2024-04-10 14:09:39 -0500

    use x shape

--- utils/train.py
+++ utils/train.py
@@ -121,7 +121,7 @@ def noisy_onet_epoch(
         xs, ys = batch
         optimizer.zero_grad()
         for x, y in zip(xs,ys):
-            noise = noise_variance * torch.tensor(np.random.randn(x.shape[0]))
+            noise = noise_variance * torch.tensor(np.random.randn(x.shape))
             noisy_x = x + noise
             times = torch.linspace(0,1,285)[:,None]
             pred_y = model(noisy_x, times)
