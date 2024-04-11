a11d1631f9fefc2547947745ce9824e2463de296
wglao <wesleygarlao@gmail.com>
2024-04-11 11:15:30 -0500

    debug size

--- utils/train.py
+++ utils/train.py
@@ -40,12 +40,13 @@ def basis_ortho_loss(o_net_model, t, dt: float = 1.0):
     time_integrate[0] = 0.5 * dt
     time_integrate[-1] = 0.5 * dt
     inner_weights = torch.diag(time_integrate)
+    breakpoint()
     v = o_net_model.trunk_net(t)
     inner_products = v.T @ inner_weights @ v
     up_ids = np.triu_indices(v.shape[1],1)
     # diag_ids = np.diag_indices(v.shape[1],2)
 
-    loss_value = inner_products[up_ids]
+    loss_value = torch.sum(inner_products[up_ids])
     return loss_value
 
 def naive_mlp_epoch(
